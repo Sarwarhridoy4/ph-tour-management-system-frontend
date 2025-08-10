c892301fc5feb1dd64a55bbdebaf9afb2a6fa2e0
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-08-10 11:42:19 +0600

    fix:: google login open new tab - replace window.open with window.location.href

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -19,6 +19,7 @@ import {
   useUserInfoQuery,
 } from "@/redux/features/auth/auth.api";
 import { useAppDispatch } from "@/redux/hook";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -30,7 +31,6 @@ export default function Navbar() {
   const { data } = useUserInfoQuery(undefined);
   const [logout] = useLogoutMutation();
   const dispatch = useAppDispatch();
-  console.log(data?.data?.email);
 
   const handleLogout = async () => {
     await logout(undefined);
@@ -38,51 +38,51 @@ export default function Navbar() {
   };
 
   return (
-    <header className="border-b">
-      <div className="container mx-auto px-4 flex h-16 items-center justify-between gap-4">
+    <header className='border-b'>
+      <div className='container mx-auto px-4 flex h-16 items-center justify-between gap-4'>
         {/* Left side */}
-        <div className="flex items-center gap-2">
+        <div className='flex items-center gap-2'>
           {/* Mobile menu trigger */}
           <Popover>
             <PopoverTrigger asChild>
               <Button
-                className="group size-8 md:hidden"
-                variant="ghost"
-                size="icon"
+                className='group size-8 md:hidden'
+                variant='ghost'
+                size='icon'
               >
                 <svg
-                  className="pointer-events-none"
+                  className='pointer-events-none'
                   width={16}
                   height={16}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  xmlns='http://www.w3.org/2000/svg'
                 >
                   <path
-                    d="M4 12L20 12"
-                    className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                    d='M4 12L20 12'
+                    className='origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]'
                   />
                   <path
-                    d="M4 12H20"
-                    className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                    d='M4 12H20'
+                    className='origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45'
                   />
                   <path
-                    d="M4 12H20"
-                    className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                    d='M4 12H20'
+                    className='origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]'
                   />
                 </svg>
               </Button>
             </PopoverTrigger>
-            <PopoverContent align="start" className="w-36 p-1 md:hidden">
-              <NavigationMenu className="max-w-none *:w-full">
-                <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+            <PopoverContent align='start' className='w-36 p-1 md:hidden'>
+              <NavigationMenu className='max-w-none *:w-full'>
+                <NavigationMenuList className='flex-col items-start gap-0 md:gap-2'>
                   {navigationLinks.map((link, index) => (
-                    <NavigationMenuItem key={index} className="w-full">
-                      <NavigationMenuLink asChild className="py-1.5">
+                    <NavigationMenuItem key={index} className='w-full'>
+                      <NavigationMenuLink asChild className='py-1.5'>
                         <Link to={link.href}>{link.label} </Link>
                       </NavigationMenuLink>
                     </NavigationMenuItem>
@@ -92,18 +92,18 @@ export default function Navbar() {
             </PopoverContent>
           </Popover>
           {/* Main nav */}
-          <div className="flex items-center gap-6">
-            <a href="#" className="text-primary hover:text-primary/90">
+          <div className='flex items-center gap-6'>
+            <a href='#' className='text-primary hover:text-primary/90'>
               <Logo />
             </a>
             {/* Navigation menu */}
-            <NavigationMenu className="max-md:hidden">
-              <NavigationMenuList className="gap-2">
+            <NavigationMenu className='max-md:hidden'>
+              <NavigationMenuList className='gap-2'>
                 {navigationLinks.map((link, index) => (
                   <NavigationMenuItem key={index}>
                     <NavigationMenuLink
                       asChild
-                      className="text-muted-foreground hover:text-primary py-1.5 font-medium"
+                      className='text-muted-foreground hover:text-primary py-1.5 font-medium'
                     >
                       <Link to={link.href}>{link.label}</Link>
                     </NavigationMenuLink>
@@ -114,20 +114,26 @@ export default function Navbar() {
           </div>
         </div>
         {/* Right side */}
-        <div className="flex items-center gap-2">
+        <div className='flex items-center gap-2'>
+          {data?.data?.picture && (
+            <Avatar>
+              <AvatarImage src={data?.data?.picture} alt='@shadcn' />
+              <AvatarFallback>GU</AvatarFallback>
+            </Avatar>
+          )}
           <ModeToggle />
           {data?.data?.email && (
             <Button
               onClick={handleLogout}
-              variant="outline"
-              className="text-sm"
+              variant='outline'
+              className='text-sm'
             >
               Logout
             </Button>
           )}
           {!data?.data?.email && (
-            <Button asChild className="text-sm">
-              <Link to="/login">Login</Link>
+            <Button asChild className='text-sm'>
+              <Link to='/login'>Login</Link>
             </Button>
           )}
         </div>
--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -52,24 +52,24 @@ export function LoginForm({
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
-        <p className="text-balance text-sm text-muted-foreground">
+      <div className='flex flex-col items-center gap-2 text-center'>
+        <h1 className='text-2xl font-bold'>Login to your account</h1>
+        <p className='text-balance text-sm text-muted-foreground'>
           Enter your email below to login to your account
         </p>
       </div>
-      <div className="grid gap-6">
+      <div className='grid gap-6'>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
             <FormField
               control={form.control}
-              name="email"
+              name='email'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
-                      placeholder="john@example.com"
+                      placeholder='john@example.com'
                       {...field}
                       value={field.value || ""}
                     />
@@ -81,14 +81,14 @@ export function LoginForm({
 
             <FormField
               control={form.control}
-              name="password"
+              name='password'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
                     <Input
-                      type="password"
-                      placeholder="********"
+                      type='password'
+                      placeholder='********'
                       {...field}
                       value={field.value || ""}
                     />
@@ -98,31 +98,33 @@ export function LoginForm({
               )}
             />
 
-            <Button type="submit" className="w-full">
+            <Button type='submit' className='w-full'>
               Login
             </Button>
           </form>
         </Form>
 
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+        <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
+          <span className='relative z-10 bg-background px-2 text-muted-foreground'>
             Or continue with
           </span>
         </div>
 
         {/*//* http://localhost:5000/api/v1/auth/google */}
         <Button
-          onClick={() => window.open(`${config.baseUrl}/auth/google`)}
-          type="button"
-          variant="outline"
-          className="w-full cursor-pointer"
+          onClick={() => {
+            window.location.href = `${config.baseUrl}/auth/google`;
+          }}
+          type='button'
+          variant='outline'
+          className='w-full cursor-pointer'
         >
           Login with Google
         </Button>
       </div>
-      <div className="text-center text-sm">
+      <div className='text-center text-sm'>
         Don&apos;t have an account?{" "}
-        <Link to="/register" replace className="underline underline-offset-4">
+        <Link to='/register' replace className='underline underline-offset-4'>
           Register
         </Link>
       </div>
